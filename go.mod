f9b383f742f3df14668b46d543f333f0d5c6235e
DadangRokes <31819806+badsector998@users.noreply.github.com>
2022-04-20 01:18:42 +0700

    GO-OLE Version

--- go.mod
+++ go.mod
@@ -1,11 +1,10 @@
-module wincc.go
+module ole-test
 
 go 1.17
 
-require github.com/mattn/go-adodb v0.0.1
-
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	github.com/mattn/go-adodb v0.0.1 // indirect
+	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"time"
 
 	"github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
 	_ "github.com/mattn/go-adodb"
 )
 
@@ -15,66 +14,100 @@ const (
 	DSN      = "Catalog=CC_OS_1__21_12_14_16_25_11R;"
 	DS       = "Data Source=10.1.1.1\\WINCC"
 	db_type  = "adodb"
+	DDSN     = "Catalog=CC_OS_1__21_12_14_16_25_11R"
 )
 
 func main() {
-	fmt.Println("Coba Koneksi WinCC DB!")
+	fmt.Println("OLE TEST PROGRAM")
+
 	ole.CoInitialize(0)
 	defer ole.CoUninitialize()
-	conn_string := Provider + DSN + DS
-	db, err := sql.Open(db_type, conn_string)
-	if err != nil {
-		fmt.Println("open", err)
-		delay()
-		return
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Ping error : ", err)
-		delay()
-		return
-	}
-
-	// command, err := oleutil.CreateObject("ADODB.Command")
 
 	query := `TAG:R,'SystemArchive\1511_AT_1379A/MS.PV_Out#Value','2022-02-08 07:35:00.000', '2022-02-08 07:40:00.000'`
-	row, err := db.Query(query)
-	if err != nil {
-		fmt.Println("Query Error : ", err)
-		delay()
-		return
-	}
-	defer row.Close()
-	for row.Next() {
-		var (
-			valueId   int
-			timeStamp string
-			realValue string
-			quality   string
-			flags     string
-		)
-		// var valueName string
-		err = row.Scan(&valueId, &timeStamp, &realValue, &quality, &flags)
-		if err != nil {
-			fmt.Println("Row Error : ", err)
-		}
-		fmt.Println(valueId, timeStamp, realValue, quality, flags)
-	}
+	conn_string := Provider + DSN + DS
 
-	err = row.Err()
+	// db, err := sql.Open(db_type, conn_string)
+	// if err != nil {
+	// 	fmt.Println("open", err)
+	// 	delay()
+	// 	return
+	// }
+	// defer db.Close()
+
+	conn_service, err := oleutil.CreateObject("ADODB.Connection")
 	if err != nil {
-		fmt.Println("Error row : ", err)
-		delay()
+		fmt.Println("Error Creating Connection Object", err)
 		return
 	}
 
-	duration := time.Duration(10) * time.Second
-	time.Sleep(duration)
+	db, err := conn_service.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		fmt.Println("Error DB Connection", err)
+		return
+	}
+
+	_, err = oleutil.CallMethod(db, "Open", DDSN)
+	if err != nil {
+		fmt.Println("Error DB Connection on Ping", err)
+		return
+	}
+
+	cmd, err := oleutil.CreateObject("ADODB.Command")
+	if err != nil {
+		fmt.Print("Error Creating Command Object", err)
+		return
+	}
+
+	sq, err := cmd.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		fmt.Println("Error Creating Query Interface", err)
+		return
+	}
+	defer sq.Release()
+
+	_, err = oleutil.PutProperty(sq, "ActiveConnection", db)
+	if err != nil {
+		fmt.Println("Error Allocating Active Connection", err)
+		return
+	}
+
+	_, err = oleutil.PutProperty(sq, "CommandText", query)
+	if err != nil {
+		fmt.Println("Error Preparing Query", err)
+		return
+	}
+
+	_, err = oleutil.PutProperty(sq, "CommandType", 1)
+	if err != nil {
+		fmt.Println("Error Preparing Command Type", err)
+		return
+	}
+
+	_, err = oleutil.PutProperty(sq, "Prepared", true)
+	if err != nil {
+		fmt.Println("Error Prepared Statement", err)
+		return
+	}
+
+	val, err := oleutil.GetProperty(sq, "Parameters")
+	if err != nil {
+		fmt.Println("Error Getting SQ Params", err)
+		return
+	}
+
+	fmt.Println(val)
+
+	result, err := oleutil.CallMethod(sq, "ExecQuery", conn_string)
+	if err != nil {
+		fmt.Println("Result error", err)
+		return
+	}
+
+	fmt.Println(result)
+
 }
 
-func delay() {
-	duration := time.Duration(10) * time.Second
-	time.Sleep(duration)
-}
+// func delay() {
+// 	duration := time.Duration(10) * time.Second
+// 	time.Sleep(duration)
+// }
